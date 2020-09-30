722af9186b1a68e91a61fcf3ec45c80d99155d11
Cameron Kleiman <camtheman256@gmail.com>
2020-09-29 19:27:17 -0500

    Add uuids and separate date component

--- src/SchedulesEditorComponent.tsx
+++ src/SchedulesEditorComponent.tsx
@@ -3,6 +3,8 @@ import { Accordion, Alert, Badge, Button, Card, Form } from "react-bootstrap";
 import { parse } from "@prantlf/jsonlint";
 import { Period, Schedule } from "./schedule";
 import PeriodsEditorComponent from './PeriodsEditorComponent';
+import DateArrayComponent from "./DateArrayComponent";
+import { v4 as newId } from "uuid";
 
 interface SchedulesEditorProps {
   onChange: Function,
@@ -125,18 +127,18 @@ function ScheduleEditorComponent(props: ScheduleEditorProps) {
               <Alert variant="info">This schedule is the default schedule, so it's always used when a special schedule is not in place.</Alert>
             ) : null}
             <Form>
-              <Form.Group controlId={`name-${props.index}`}>
+              <Form.Group controlId={newId()}>
                 <Form.Label>Schedule Name</Form.Label>
                 <Form.Control type="text" placeholder="Enter a name" value={props.schedule.name} onChange={onScheduleNameChange}></Form.Control>
               </Form.Group>
-              <Form.Check checked={props.schedule.combinedAB || false} label="Combined A/B Periods?" onChange={onScheduleABChange} type="checkbox"
-                id={`combined-${props.index}`}
+              <Form.Check checked={props.schedule.combinedAB || false} label="Combined A/B Period Names?" onChange={onScheduleABChange} type="checkbox"
+                id={newId()}
               ></Form.Check>
               <Form.Check checked={props.schedule.applyDay !== undefined} label="Apply schedule on a certain day of the week?" type="checkbox"
-                id={`apply-on-${props.index}`} onChange={changeApplyDayVisibility}
+                id={newId()} onChange={changeApplyDayVisibility}
               ></Form.Check>
               {props.schedule.applyDay !== undefined ? (
-                <Form.Control as="select" value={props.schedule.applyDay} id={`apply-${props.index}`} onChange={setApplyDay}>
+                <Form.Control as="select" value={props.schedule.applyDay} id={newId()} onChange={setApplyDay}>
                   {daysOfWeek.map((d, i) => (<option value={(i+1).toString()} key={i}>{i+1} - {d}</option>))}
                 </Form.Control>
               ) : null}
@@ -148,80 +150,6 @@ function ScheduleEditorComponent(props: ScheduleEditorProps) {
           </Card.Body>
         </Accordion.Collapse>
       </Card>
-    </>
-  );
-}
-
-interface DateArrayComponentProps {
-  onChange: Function,
-  value?: string[],
-  index: number
-}
-
-function DateArrayComponent(props: DateArrayComponentProps) {
-  function toDateValue(valueString: string): string {
-    const dateObject = new Date(valueString);
-    return !isNaN(dateObject as any) ? dateObject.toISOString().split("T")[0] : "";
-  }
-
-  function onDateKeyEdit(indices: number[], event: any) {
-    if(props.value) {
-      const newValue = event.target.valueAsDate.toLocaleDateString(undefined, { timeZone: "UTC" });
-      if(indices.length > 1) {
-        // @ts-ignore
-        props.value[indices[0]][indices[1]] = newValue;
-      } else {
-        props.value[indices[0]] = newValue;
-      }
-      props.onChange(props.value);
-    }
-  }
-
-  function removeDateKey(index: number) {
-    if(props.value) {
-      props.value.splice(index, 1);
-      props.onChange(props.value);
-    }
-  }
-
-  function newDateRange() {
-    if(props.value) {
-      // @ts-ignore
-      props.value.push(["", ""]);
-    }
-    props.onChange(props.value ?? [["", ""]]);
-  }
-
-  function newDate() {
-    if(props.value) {
-      props.value.push("");
-    }
-    props.onChange(props.value ?? [""]);
-  }
-  
-  return (
-    <>
-      <h5>Dates:</h5>
-      {props.index === 0 ? (
-        <Alert variant="secondary">Dates have no effect on the default schedule. Feel free to ignore.</Alert>
-      ) : null}
-      {props.value ? props.value.map((v: string | string[], i) => (
-        <div key={i} className="mb-2">
-          {v instanceof Array ? (
-            <>
-              {toDateValue(v[1]) <= toDateValue(v[0]) ? (
-                <Alert variant="warning">Your end date is before or on your start date.</Alert>
-              ) : null}
-              <Form.Control type="date" value={toDateValue(v[0])} className="inline-auto" onChange={(e) => onDateKeyEdit([i, 0], e)}></Form.Control> - <Form.Control type="date" value={toDateValue(v[1])} className="inline-auto" onChange={(e) => onDateKeyEdit([i, 1], e)}></Form.Control>
-            </>
-          ) : (
-            <Form.Control type="date" value={toDateValue(v)} className="inline-auto" onChange={(e) => onDateKeyEdit([i], e)}></Form.Control>
-          )}
-          <Button variant="danger" className="ml-3" title="Remove" onClick={() => removeDateKey(i)}>&times;</Button>
-        </div>
-      )) : null}
-      <Button variant="primary" onClick={() => newDate()}>+ Add new date</Button>
-      <Button variant="primary" className="ml-3" onClick={() => newDateRange()}>+ Add new date range</Button>
     </>
   );
 }
